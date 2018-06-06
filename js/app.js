8cc8ce7b836537fc0d56d4068a4b1c9a8adbfb25
svalls <34880594+svalls@users.noreply.github.com>
2018-06-06 10:12:22 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
-
+var map;
+      function initMap() {
+        // Constructor creates a new map - only center and zoom are required.
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 34.1367208, lng: -118.6614809},
+          zoom: 13
+        });
+      }

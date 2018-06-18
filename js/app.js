9b26f38904b38896d2a18b9a65c231dfc9f52577
svalls <34880594+svalls@users.noreply.github.com>
2018-06-18 14:49:11 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -64,6 +64,14 @@ function initMap() {
   var largeInfowindow = new google.maps.InfoWindow();
   var bounds = new google.maps.LatLngBounds();
 
+
+  // MARKER DEFAULT COLOR
+  var defaultIcon = makeMarkerIcon('009999');
+
+  // MARKER MOUSEOVER COLOR
+  var highlightedIcon = makeMarkerIcon('FFFF24');
+
+
   // The following group uses the location array to create an array of markers on initialize.
   for (var i = 0; i < locations.length; i++) {
     // Get the position from the location array.
@@ -75,23 +83,38 @@ function initMap() {
       position: position,
       title: title,
       animation: google.maps.Animation.DROP,
+      icon: defaultIcon,
       id: i
     });
+    
     // Push the marker to our array of markers.
     markers.push(marker);
+    
     // Create an onclick event to open an infowindow at each marker.
     marker.addListener('click', function() {
-      populateInfoWindow(this, largeInfowindow);
+    populateInfoWindow(this, largeInfowindow);
     });
     bounds.extend(markers[i].position);
   }
-  // Extend the boundaries of the map for each marker
-  map.fitBounds(bounds);
-}
+    
+    // Extend the boundaries of the map for each marker
+    map.fitBounds(bounds);
 
-// This function populates the infowindow when the marker is clicked. We'll only allow
-// one infowindow which will open at the marker that is clicked, and populate based
-// on that markers position.
+    // MARKER EVENT LISTENER MOUSEOVER
+    marker.addListener('mouseover', function() {
+      this.setIcon(highlightedIcon);
+    });
+    
+    // MARKER EVENT LISTENER MOUSEOUT
+    marker.addListener('mouseout', function() {
+      this.setIcon(defaultIcon);
+    });
+
+} //end initmap function
+
+
+
+// DISPLAYS INFOWINDOW WHEN MARKER IS CLICKED
 function populateInfoWindow(marker, infowindow) {
   // Check to make sure the infowindow is not already opened on this marker.
   if (infowindow.marker != marker) {
@@ -103,6 +126,24 @@ function populateInfoWindow(marker, infowindow) {
       infowindow.setMarker = null;
     });
   }
-} //end initmap function
+} //end populateInfoWindow
+
+
+
+// This function takes in a COLOR, and then creates a new marker
+// icon of that color. The icon will be 21 px wide by 34 high, have an origin
+// of 0, 0 and be anchored at 10, 34).
+function makeMarkerIcon(markerColor) {
+  console.log('makeMarkerIcon function');
+  var markerImage = new google.maps.MarkerImage(
+    'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+    '|40|_|%E2%80%A2',
+    new google.maps.Size(21, 34),
+    new google.maps.Point(0, 0),
+    new google.maps.Point(10, 34),
+    new google.maps.Size(21,34));
+  return markerImage;
+} //end makeMarkerIcon function
+
 
 
